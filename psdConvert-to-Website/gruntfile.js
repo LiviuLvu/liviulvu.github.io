80dbb0a9d831b8232a2c89dd64c281d56cfd7dd0
Liviu Iancu <lvudesign@gmail.com>
2016-01-17 01:35:17 +0200

    Update to fluid layout
    Getting closer to goal. Only the menu needs to adapt to small screens.

--- psdConvert-to-Website/gruntfile.js
+++ psdConvert-to-Website/gruntfile.js
@@ -1,43 +1,43 @@
-module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        //read all plugins added in the package.json file
-        pkg: grunt.file.readJSON('package.json'),
+module.exports = function(grunt) {
+   // Project configuration.
+   grunt.initConfig({
+      //read all plugins added in the package.json file
+      pkg: grunt.file.readJSON('package.json'),
 
-        stylus: {
-            compile: {
-                options: {
-                    compress: false
-                },
-                files: {
-                    'css/style.css' : 'css/style.styl'
-                }
-            }
-        },
-
-        watch: {
-            files: ['css/*.styl','index.html'],
-            tasks: ['stylus'],
-        },
-
-        browserSync: {
-            bsFiles: {
-                src : ['css/*.css','index.html']
-            },
+      stylus: {
+         compile: {
             options: {
-                watchTask: true,
-                server: {
-                    baseDir: "g:/node/http-server/public/stylus/stylus-page"
-                }
+               compress: false
+            },
+            files: {
+               'css/style.css': 'css/style.styl'
             }
-        },
+         }
+      },
 
-    });
-    // register grunt plugins below
-    grunt.loadNpmTasks('grunt-contrib-stylus');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    // browser automatic reload on file change
-    grunt.loadNpmTasks('grunt-browser-sync');
-    //this default task runs when using 'grunt' command in terminal
-    grunt.registerTask('default', ['browserSync', 'watch']);
+      watch: {
+         files: ['css/*.styl', 'index.html'],
+         tasks: ['stylus'],
+      },
+
+      browserSync: {
+         bsFiles: {
+            src: ['css/*.css', 'index.html']
+         },
+         options: {
+            watchTask: true,
+            server: {
+               baseDir: "g:/node/http-server/public/stylus/psdConvert-to-Website"
+            }
+         }
+      },
+
+   });
+   // register grunt plugins below
+   grunt.loadNpmTasks('grunt-contrib-stylus');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   // browser automatic reload on file change
+   grunt.loadNpmTasks('grunt-browser-sync');
+   //this default task runs when using 'grunt' command in terminal
+   grunt.registerTask('default', ['browserSync', 'watch']);
 };
