b85eae8d211b77f7f9ce53bc18ea8dafb335f73b
Liviu Iancu <lvudesign@gmail.com>
2016-01-16 21:51:43 +0200

    converting a psd file to a responsive web page
    Manually converting a psd to website, learning about best practices.
    First time using Stylus css
    Work in progress

---
+++ psdConvert-to-Website/README.md
@@ -0,0 +1,16 @@
+# Study conversion from psd file to responsive page
+
+## Work in progress
+
+Started this project with purpouse of learning the best practices in HTML5 CSS3 front end worflow, building the most lightweight mobile friendly and fast loading mockup page.
+
+Using grunt task manager, stylus css, browser-sync
+
+First time using a css pre-processor - stylus. Really easy to use an very light on typing.
+
+Live preview:
+http://liviulvu.github.io/psdConvert-to-Website
+
+This is how the page looks like
+![image](https://raw.githubusercontent.com/LiviuLvu/meteor/master/psdConvert-to-Website/preview.png)
+
---
+++ psdConvert-to-Website/gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        //read all plugins added in the package.json file
+        pkg: grunt.file.readJSON('package.json'),
+
+        stylus: {
+            compile: {
+                options: {
+                    compress: false
+                },
+                files: {
+                    'css/style.css' : 'css/style.styl'
+                }
+            }
+        },
+
+        watch: {
+            files: ['css/*.styl','index.html'],
+            tasks: ['stylus'],
+        },
+
+        browserSync: {
+            bsFiles: {
+                src : ['css/*.css','index.html']
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: "g:/node/http-server/public/stylus/stylus-page"
+                }
+            }
+        },
+
+    });
+    // register grunt plugins below
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // browser automatic reload on file change
+    grunt.loadNpmTasks('grunt-browser-sync');
+    //this default task runs when using 'grunt' command in terminal
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
