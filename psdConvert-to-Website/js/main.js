917dc4b6f66ef201d5574fb35c5432e26c76f420
Liviu Iancu <lvudesign@gmail.com>
2016-02-20 14:35:42 +0200

    moving mobile page to separate repo

--- psdConvert-to-Website/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-   $('.icon-menu').on('click', function() {
-      //show menu items
-      $(this).siblings().toggleClass('a-container-show');
-      //toggle between menu icon and close icon
-      $(this).toggleClass('close thick');
-   });
-});
