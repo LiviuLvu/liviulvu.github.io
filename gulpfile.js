d1da93e6d6ef203a9f6d1b3ba415f88b058a95d9
Liviu Iancu <liviuiancu@gmx.com>
2016-12-21 20:46:56 +0200

    updated layout and text, simplified message

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,37 @@ var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 // minify
 var cssnano = require('gulp-cssnano');
+// run tasks in order
+var runSequence = require('run-sequence');
 
 gulp.task('sass', function(){
-   return gulp.src('developement/style/*.scss')
-   .pipe(sass())
-   .pipe(gulp.dest('style'))
-   .pipe(browserSync.reload({
-      stream: true
-   }));
-});
-gulp.task('browserSync', function() {
-   browserSync.init({
-      server: {
-         baseDir: './'
-      },
-   });
-});
-gulp.task('useref', function() {
-   return gulp.src('developement/*.html')
-   .pipe(useref())
-   .pipe(gulpIf('*.css', cssnano()))
-   .pipe(gulp.dest('./'));
+ return gulp.src('developement/style/*.scss')
+ .pipe(sass())
+ .pipe(gulp.dest('style'));
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'useref'], function() {
-   gulp.watch('developement/style/*.scss',['sass', 'useref']);
-   gulp.watch('./*.html', browserSync.reload);
+gulp.task('browserSync', function() {
+ browserSync.init({
+  server: {
+   baseDir: './'
+  },
+ });
 });
+
+gulp.task('useref', function() {
+ return gulp.src('developement/*.html')
+ .pipe(useref())
+ .pipe(gulpIf('*.css', cssnano()))
+ .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', function(callback) {
+ runSequence('sass','useref', 'watch', callback);
+});
+
+gulp.task('watch', function() {
+ gulp.watch('developement/style/*.scss', ['build']);
+ gulp.watch('developement/*.html', ['build']);
+});
+
+gulp.task('default',['watch','browserSync']);
